1a4611f111a00dd97f3a0facf0e9230866e9694a
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-10-26 18:46:03 -0400

    made 2 more sections

---
+++ src/components/mission.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+
+<div class="bg-primary p-10">
+    <div class="m-auto">
+        <img src="/homepage-redesign/CCHSwithSealMaskedWhite.png" alt="">
+    </div>
+    <h1 class="text-center font-headliner text-3xl lg:text-7xl font-bold text-white">
+        Educating diverse leaders since 1887, Camden Catholic develops tomorrow's innovative leaders steeped in the Catholic tradition of mercy.
+    </h1>
+</div>
+
+<style>
+    h1 {
+        line-height: 1.1;
+    }
+    img {
+        width: 40vw;
+        margin: auto;
+    }
+</style>
---
+++ src/components/whoIs.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<div class="p-24">
+    <h1 class="text-center font-headliner text-3xl lg:text-6xl font-semibold text-primary">
+        Who is CCHS
+    </h1>
+    <hr class="p-4" />
+    
+    <p class="text-center font-secondary text-xl lg:text-3xl text-black p-4">
+        Camden Catholic High School is a suburban, college preparatory, day and boarding school. It educates young men and women spiritually, academically, socially and physically in a faith community to develop as life long learners and leaders in a global society in the Catholic tradition.
+    </p>
+    <div class="flex content-center justify-center">
+        <a href="#" class="rounded-md bg-primary p-2 text-CCHSwhite">Learn More</a>
+    </div>
+</div>
